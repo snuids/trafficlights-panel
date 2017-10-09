f87a5d0ed607e6b68a6258529b6b694f671b2d4c
snuids <a.marchand@progeri.com>
2017-10-09 16:00:41 +0200

    Save point

---
+++ dist/trafficlight_ctrl.js
@@ -0,0 +1,198 @@
+'use strict';
+
+System.register(['app/plugins/sdk', 'moment', 'lodash', 'app/core/time_series', './css/trafficlight-panel.css!'], function (_export, _context) {
+  "use strict";
+
+  var MetricsPanelCtrl, moment, _, TimeSeries, _createClass, panelDefaults, TrafficLightCtrl;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _possibleConstructorReturn(self, call) {
+    if (!self) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return call && (typeof call === "object" || typeof call === "function") ? call : self;
+  }
+
+  function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value: subClass,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  }
+
+  return {
+    setters: [function (_appPluginsSdk) {
+      MetricsPanelCtrl = _appPluginsSdk.MetricsPanelCtrl;
+    }, function (_moment) {
+      moment = _moment.default;
+    }, function (_lodash) {
+      _ = _lodash.default;
+    }, function (_appCoreTime_series) {
+      TimeSeries = _appCoreTime_series.default;
+    }, function (_cssTrafficlightPanelCss) {}],
+    execute: function () {
+      _createClass = function () {
+        function defineProperties(target, props) {
+          for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+          }
+        }
+
+        return function (Constructor, protoProps, staticProps) {
+          if (protoProps) defineProperties(Constructor.prototype, protoProps);
+          if (staticProps) defineProperties(Constructor, staticProps);
+          return Constructor;
+        };
+      }();
+
+      panelDefaults = {
+        bgColor: null,
+        trafficLightSettings: {
+          lightsPerLine: 5,
+          width: 20,
+          invertScale: false,
+          showValue: true,
+          redThreshold: 80,
+          greenThreshold: 20,
+          max: 100,
+          fontSize: '12px'
+        }
+      };
+
+      _export('TrafficLightCtrl', TrafficLightCtrl = function (_MetricsPanelCtrl) {
+        _inherits(TrafficLightCtrl, _MetricsPanelCtrl);
+
+        function TrafficLightCtrl($scope, $injector) {
+          _classCallCheck(this, TrafficLightCtrl);
+
+          var _this = _possibleConstructorReturn(this, (TrafficLightCtrl.__proto__ || Object.getPrototypeOf(TrafficLightCtrl)).call(this, $scope, $injector));
+
+          _.defaultsDeep(_this.panel, panelDefaults);
+
+          _this.events.on('init-edit-mode', _this.onInitEditMode.bind(_this));
+          _this.events.on('panel-teardown', _this.onPanelTeardown.bind(_this));
+          _this.events.on('panel-initialized', _this.render.bind(_this));
+
+          _this.events.on('render', _this.onRender.bind(_this));
+          _this.events.on('data-received', _this.onDataReceived.bind(_this));
+          _this.events.on('data-error', _this.onDataError.bind(_this));
+          _this.events.on('data-snapshot-load', _this.onDataReceived.bind(_this));
+
+          _this.percentPerLight = 100;
+
+          _this.data = [];
+          _this.updateTraffics();
+          return _this;
+        }
+
+        _createClass(TrafficLightCtrl, [{
+          key: 'onDataError',
+          value: function onDataError() {
+            this.series = [];
+            this.render();
+          }
+        }, {
+          key: 'onRender',
+          value: function onRender() {
+            //this.data = this.parseSeries(this.series);
+            console.log("On Render");
+          }
+        }, {
+          key: 'onDataReceived',
+          value: function onDataReceived(dataList) {
+            this.series = dataList.map(this.seriesHandler.bind(this));
+
+            var newseries = [];
+            for (var i = 0; i < this.series.length; i++) {
+              var newserie = {
+                "name": this.series[i].label,
+                "value": this.series[i].datapoints.slice(-1)[0][0]
+              };
+              newseries.push(newserie);
+            }
+            console.log(JSON.stringify(newseries));
+
+            if (this.panel.trafficLightSettings.invertScale) this.data = _.orderBy(newseries, 'value', 'desc');else this.data = _.orderBy(newseries, 'value', 'asc');
+          }
+        }, {
+          key: 'seriesHandler',
+          value: function seriesHandler(seriesData) {
+            var series = new TimeSeries({
+              datapoints: seriesData.datapoints,
+              alias: seriesData.target
+            });
+            return series;
+          }
+        }, {
+          key: 'onInitEditMode',
+          value: function onInitEditMode() {
+
+            this.addEditorTab('Options', 'public/plugins/grafana-traffic-lights/editor.html', 2);
+          }
+        }, {
+          key: 'onPanelTeardown',
+          value: function onPanelTeardown() {
+            this.$timeout.cancel(this.nextTickPromise);
+          }
+        }, {
+          key: 'updateTraffics',
+          value: function updateTraffics() {
+            this.percentPerLight = 100 / this.panel.trafficLightSettings.lightsPerLine;
+
+            this.lines = [];
+            var metrics = [];
+            for (var i = 0; i < this.data.length; i++) {
+              if (i % this.panel.trafficLightSettings.lightsPerLine == 0) {
+                metrics = [];
+                this.lines.push(metrics);
+              }
+              metrics.push(this.data[i]);
+            }
+            this.nextTickPromise = this.$timeout(this.updateTraffics.bind(this), 1000);
+          }
+        }, {
+          key: 'link',
+          value: function link(scope, elem) {
+            var _this2 = this;
+
+            this.events.on('render', function () {
+              var $panelContainer = elem.find('.panel-container');
+
+              if (_this2.panel.bgColor) {
+                $panelContainer.css('background-color', _this2.panel.bgColor);
+              } else {
+                $panelContainer.css('background-color', '');
+              }
+            });
+          }
+        }]);
+
+        return TrafficLightCtrl;
+      }(MetricsPanelCtrl));
+
+      _export('TrafficLightCtrl', TrafficLightCtrl);
+
+      TrafficLightCtrl.templateUrl = 'module.html';
+    }
+  };
+});
+//# sourceMappingURL=trafficlight_ctrl.js.map
